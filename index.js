6e79195aef2a50785dac78a05bb922522649d6bd
davehumanitec <107107966+davehumanitec@users.noreply.github.com>
2022-06-10 15:13:26 +0200

    Update index.js
    text change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello team!\n'
+  const msg = 'Hello quick time!\n'
   res.end(msg);
 });
 
